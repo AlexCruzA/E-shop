f171604df355967e00188d3a34eb0a22ae21870e
Alex <alexander09cruz@hotmail.com>
2018-11-15 00:52:53 -0600

    Edicion de diseño en el carrito de compras

--- carrito_compras/pay.php
+++ carrito_compras/pay.php
@@ -1,63 +1,62 @@
 <?php
   $titulo = 'Facturación ';
-  include '../seguridad/verificar_session.php';
   include '../DbSetup.php'; 
   include '../shared/header.php';
   include '../shared/nav.php';
- 
+  error_reporting(0);
   $carrito= $carrito_model->getIdCarrito($user['id']);
   $search = isset($_GET['search']) ? $_GET['search'] : '';
   $user = $usuario_model->findUser($_SESSION['usuario_id']);
-  $carrito= $carrito_model->getIdCarrito($user['id']);
-
+  //$carrito= $carrito_model->getIdCarrito($user['id']);
   if($_SERVER['REQUEST_METHOD'] == 'POST'){
     $orden_model->insert($carrito['id']);
     $carrito_model->deleteAll($carrito['id']);
-    return header("Location: /carrito_compras");
+    return header("Location: /home");
   }
 ?>
 
-<!DOCTYPE html>
-  <html>
-    <head>
-      <?php  $result_array = $carrito_model->pay($carrito['id']);
-      foreach ($result_array as $row) {
-      } ?>
-      <title>Eliminar Articulo</title>
-    </head>
-    <body class="text-center">
-      <h2>Confirmar compra</h2>
-      <table align="center" border="3">
-    <tr>
-      <th class="text-center">Nombre</th>
-      <th class="text-center">Descripcion</th>
-      <th class="text-center">Precio</th>
-      <th class="text-center">Cantidad</th>
-     </tr>
-    <?php 
-      $result_array = $carrito_model->index2($search,$carrito['id']);
-      foreach ($result_array as $row) {
-        $articulo= $articulo_model->getArticuloById($row['idArticulo']);
-        echo "<tr>";
-          echo "<td>" . $articulo['nombre'] . "</td>";
-          echo "<td>" . $articulo['descripcion'] . "</td>";
-          echo "<td>$" . $articulo['precio'] . "</td>";
-          echo "<td>" . $row['cantidad'] . "</td>";
-        echo "</tr>";
-      } 
-    ?>
-</table>
-
-<?php 
-  $result_array = $carrito_model->pay($carrito['id']);
-  foreach ($result_array as $row) {
-  }
-?>
-    Desea realizar el pago por: <strong> $<?php echo $row['precio']; ?></strong> 
-    </p>
-    <form method="POST">
-      <input type="submit" value="Si">
-       <a href="/carrito_compras/index.php">No</a>
-    </form>
-  </body>
-</html>
+<head>
+  <?php  $result_array = $carrito_model->pay($carrito['id']);
+    foreach ($result_array as $row) {
+    }
+  ?>
+  <title>Eliminar Articulo</title>
+</head>
+<body class="text-center">
+  <h2>Confirmar compra</h2>
+  <table  class="table" align="center" border="3">
+     <thead class="thead-dark">
+      <tr>
+        <th class="text-center">Nombre</th>
+        <th class="text-center">Descripcion</th>
+        <th class="text-center">Precio</th>
+        <th class="text-center">Cantidad</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php 
+        $result_array = $carrito_model->index2($search,$carrito['id']);
+        foreach ($result_array as $row) {
+          $articulo= $articulo_model->getArticuloById($row['idArticulo']);
+          echo "<tr>";
+            echo "<td>" . $articulo['nombre'] . "</td>";
+            echo "<td>" . $articulo['descripcion'] . "</td>";
+            echo "<td>$" . $articulo['precio'] . "</td>";
+            echo "<td>" . $row['cantidad'] . "</td>";
+          echo "</tr>";
+        } 
+      ?>
+    </tbody>
+  </table>
+  <?php 
+    $result_array = $carrito_model->pay($carrito['id']);
+    foreach ($result_array as $row) {
+    }
+  ?>
+  <p>Desea realizar el pago por:</p>
+ <br><center> <input type="text"  class="form-control" style="width: 7%;" name="imagen" required autofocus value="$<?php echo $row['precio']; ?>" readonly></center><br>
+  <form method="POST">
+    <input class="btn btn-primary" type="submit" value="Si">
+    <a class="btn btn-danger" href="/carrito_compras/index.php">No</a>
+  </form>
+</body>
